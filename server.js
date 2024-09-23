f4e9401079f9a82c4c74c2d85ad57a3a26eea339
Zuet <zuet@eup.net.vn>
2024-09-23 13:45:38 +0700

    update

--- server.js
+++ server.js
@@ -2,8 +2,9 @@ const express = require("express");
 const mongoose = require("mongoose");
 const app = express();
 const swaggerUi = require("swagger-ui-express");
-const swaggerDocument = require("./swagger"); // Import Swagger
-
+const swaggerDocument = require("./swagger");
+const authRoutes = require("./routes/auth");
+const userRoutes = require("./routes/user");
 const PORT = process.env.PORT || 1403;
 const MONGO_URI =
   "mongodb+srv://zuet:dbZuetPassword@cluster0.yh11lns.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0";
@@ -14,13 +15,7 @@ mongoose
   .catch((err) => console.log(err));
 
 app.use(express.json());
-
-const authRoutes = require("./routes/auth");
 app.use("/api/auth", authRoutes);
-
-const userRoutes = require("./routes/user");
 app.use("/api/user", userRoutes);
-
 app.use("/swagger", swaggerUi.serve, swaggerUi.setup(swaggerDocument));
-
 app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
