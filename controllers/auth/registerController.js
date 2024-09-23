63e694768fbc17d3240bec019a9e6c05047637f0
Zuet <zuet@eup.net.vn>
2024-09-23 14:32:13 +0700

    update register

--- controllers/auth/registerController.js
+++ controllers/auth/registerController.js
@@ -3,8 +3,19 @@ const User = require("../../models/User");
 exports.register = async (req, res) => {
   const { username, password, email, fullName } = req.body;
   try {
+    const existingUser = await User.findOne({ $or: [{ username }, { email }] });
+    if (existingUser) {
+      if (existingUser.username === username) {
+        return res.status(400).json({ message: "Username already exists" });
+      }
+      if (existingUser.email === email) {
+        return res.status(400).json({ message: "Email already exists" });
+      }
+    }
+
     const newUser = new User({ username, password, email, fullName });
     await newUser.save();
+
     res.status(201).json({ message: "User registered successfully" });
   } catch (error) {
     res.status(400).json({ error: error.message });
